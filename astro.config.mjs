7a9e4057014a01e66f942863c386325dfd86824e
leoferreiralima <leoferreiralima@yahoo.com>
2024-04-30 15:20:52 -0300

    feat: add robots txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
 export default defineConfig({
   site: 'https://www.leoferreira.dev/',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: false
-  })],
+  integrations: [
+    mdx(),
+    sitemap({
+      changefreq: 'daily'
+    }),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    robotsTxt()
+  ],
   output: "hybrid",
   adapter: vercel({
     imageService: true
